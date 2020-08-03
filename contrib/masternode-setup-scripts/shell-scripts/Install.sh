fd33a90ab947ef2bca2d1e08300bedaec6ed7691
dzypher <65742350+getdzypher@users.noreply.github.com>
2020-08-03 20:26:16 +0800

    Update Install.sh

--- contrib/masternode-setup-scripts/shell-scripts/Install.sh
+++ contrib/masternode-setup-scripts/shell-scripts/Install.sh
@@ -6,7 +6,7 @@ sudo apt update && sudo apt upgrade -y
 echo "Downloading Shroud latest build..."
 wget -N https://github.com/ShroudXProject/Shroud/releases/download/v1.2.1/shroud-1.2.1-x86_64-linux-gnu.tar.gz
 echo "Extracting build..."
-sudo tar -c /usr/local/bin -zxvf shroud-1.2.1-x86_64-linux-gnu.tar.gz
+sudo tar -C /usr/local/bin -zxvf shroud-1.2.1-x86_64-linux-gnu.tar.gz
 echo "Setting permissions..."
 cd && sudo chmod +x /usr/local/bin/shroud*
 sudo chmod +x /usr/local/bin/tor*
@@ -17,7 +17,7 @@ echo "Setting up and enabling fail2ban..."
 sudo apt-get install fail2ban -y
 sudo ufw allow ssh
 sudo ufw allow 42998
-sudo ufw enable
+sudo ufw enable -y
 echo "Launching shroudd..."
 cd && cd /usr/local/bin
 shroudd -daemon
